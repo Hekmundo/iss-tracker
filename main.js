3654f1b6af7efe14cce5703728c82ec5b4c86951
Alex Hek <alexanderhek@gmail.com>
2021-02-27 10:02:10 +0000

    app working and rendering new iss position correctly

--- main.js
+++ main.js
@@ -8,46 +8,80 @@ import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import {Icon, Style} from 'ol/style';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {fromLonLat} from 'ol/proj';
 
+let coordinates = [0,0];
 
-const iconFeature = new Feature({
-  geometry: new Point([0, 0]),
-  // name: 'Null Island',
-  // population: 4000,
-  // rainfall: 500,
-});
+const getIcon = () => {
+  const iconFeature = new Feature({
+    geometry: new Point(fromLonLat(coordinates)),
+    name: 'iss'
+  }); 
+  const iconStyle = new Style({
+    image: new Icon({
+      src: 'img/iss.png',
+    }),
+  });
+  iconFeature.setStyle(iconStyle);
+  const iconSource = new VectorSource({
+    features: [iconFeature],
+  });
+  return new VectorLayer({
+    source: iconSource
+  });
+}
 
-const iconStyle = new Style({
-  image: new Icon({
-    anchor: [0.5, 46],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-    src: 'img/iss.png',
-  }),
-});
+const getBackground = () => {
+  return new VectorLayer({
+    source: new VectorSource({
+      format: new GeoJSON(),
+      url: './data/countries.json'
+    })
+  });
+}
 
-iconFeature.setStyle(iconStyle);
+const getMap = () => {
+  return new Map({
+    layers: [getBackground(), getIcon()],
+    target: document.getElementById('map'),
+    view: new View({
+      center: fromLonLat(coordinates),
+      zoom: 2,
+    }),
+  });
+}
 
-const iconSource = new VectorSource({
-  features: [iconFeature],
-});
+const initialize = () => {
+  fetch('http://api.open-notify.org/iss-now.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const { iss_position : { latitude, longitude } } = data;
+      coordinates[0] = longitude 
+      coordinates[1] = latitude;
+      console.log(coordinates);
+      map = getMap();
+    })
+}
 
-const iconLayer = new VectorLayer({
-  source: iconSource,
-});
+const updateMap = () => {
+  setTimeout(() => {
+    fetch('http://api.open-notify.org/iss-now.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const { iss_position : { latitude, longitude } } = data;
+      coordinates = [longitude, latitude];
+      console.log(coordinates);
 
-const backgroundLayer = new VectorLayer({
-  source: new VectorSource({
-    format: new GeoJSON(),
-    url: './data/countries.json'
-  })
-});
+      const layers = map.getLayers().getArray();
+      layers.pop();
+      layers.push(getIcon());
 
-const map = new Map({
-  layers: [backgroundLayer, iconLayer],
-  target: document.getElementById('map'),
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+      map.render();
+      updateMap(); // re-run update
+    })
+  }, 5000)
+}
+
+let map;
+initialize();
+updateMap();
